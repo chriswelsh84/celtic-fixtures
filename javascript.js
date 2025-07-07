b541aa108367190477b1f50d1b0bba521f9b48e5
chriswelsh84 <welsh.chris@gmail.com>
2025-07-07 16:30:57 +0100

    COUNTDOWN FIX

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 const fixtures = [
     { date: "2025-07-04", opponent: "Queen's Park", competition: "Friendly", venue: "Away", time: "19:45", tv: "Celtic TV", score: "1-0" },
     { date: "2025-07-08", opponent: "Cork City", competition: "Friendly", venue: "Away", time: "18:00", tv: "Celtic TV" },
+    { date: "2025-07-15", opponent: "Newcastle Utd", competition: "Friendly", venue: "Home", time: "19:45", tv: "Celtic TV" },
     { date: "2025-08-02", opponent: "St Mirren", competition: "Premiership", venue: "Home", time: "16:30", tv: "Sky Sports" },
     { date: "2025-08-31", opponent: "Rangers", competition: "Premiership", venue: "Away", time: "12:30", tv: "Sky Sports" },
     { date: "2025-09-TBC", opponent: "RB Leipzig", competition: "Champions League", venue: "Away", time: "TBC", tv: "TNT Sports" },
